be1c7e3b1f61c744327838442f6f5ac0c8884de8
Sandeep-0404 <sandeepgh2002@gmail.com>
2021-08-25 22:32:41 +0530

    resolved errors

--- app/src/main/java/com/example/fightagainstfake/Posts/Activities/ChatActivity.java
+++ app/src/main/java/com/example/fightagainstfake/Posts/Activities/ChatActivity.java
@@ -1,19 +1,16 @@
 package com.example.fightagainstfake.Posts.Activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import com.example.fightagainstfake.Chat;
-import com.example.fightagainstfake.ModelClass;
 import com.example.fightagainstfake.Posts.Adapters.ChatAdapter;
-import com.example.fightagainstfake.Posts.Adapters.NormalAdapter;
-import com.example.fightagainstfake.R;
 import com.example.fightagainstfake.UserModel;
 import com.example.fightagainstfake.databinding.ActivityChatBinding;
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,7 +25,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
-import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
     FirebaseUser user;
@@ -42,17 +38,17 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        activityChatBinding=ActivityChatBinding.inflate(getLayoutInflater());
+        activityChatBinding = ActivityChatBinding.inflate(getLayoutInflater());
         setContentView(activityChatBinding.getRoot());
         getSupportActionBar().hide();
-        intent=getIntent();
-        String userid=intent.getStringExtra("userid");
-        user= FirebaseAuth.getInstance().getCurrentUser();
-        reference= FirebaseDatabase.getInstance().getReference("Users").child(userid);
+        intent = getIntent();
+        String userid = intent.getStringExtra("userid");
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                UserModel userModel=snapshot.getValue(UserModel.class);
+                UserModel userModel = snapshot.getValue(UserModel.class);
                 activityChatBinding.userMssg.setText(userModel.getName());
             }
 
@@ -65,29 +61,39 @@ public class ChatActivity extends AppCompatActivity {
         activityChatBinding.send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String mssg=activityChatBinding.addComment.getText().toString();
-                Calendar calendar=Calendar.getInstance();
-                SimpleDateFormat dateFormat=new SimpleDateFormat("dd/MM/yy  HH:mm");
-                String datetime=dateFormat.format(calendar.getTime());
+                String mssg = activityChatBinding.addComment.getText().toString();
+                Calendar calendar = Calendar.getInstance();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy  HH:mm");
+                String datetime = dateFormat.format(calendar.getTime());
                 if (mssg.isEmpty())
                     Toast.makeText(ChatActivity.this, "Empty Message!", Toast.LENGTH_SHORT).show();
-                else
-                {
-                    if(activityChatBinding.startRv.getVisibility()==View.VISIBLE)
+                else {
+                    if (activityChatBinding.startRv.getVisibility() == View.VISIBLE)
                         activityChatBinding.startRv.setVisibility(View.INVISIBLE);
-                    Chat chat=new Chat(user.getUid(),userid,datetime,mssg);
+                    Chat chat = new Chat(user.getUid(), userid, datetime, mssg);
                     FirebaseDatabase.getInstance().getReference().child("Chats").push().setValue(chat);
+
+
+                    HashMap<String, String> chatNotification = new HashMap<>();
+                    chatNotification.put("from", user.getUid());
+                    chatNotification.put("msg", "msg");
+
+                    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Notification").child(userid);
+                    reference.push().setValue(chatNotification);
+
+
                 }
+
 
                 activityChatBinding.addComment.setText("");
 
             }
         });
 
-        layoutManager=new LinearLayoutManager(this);
+        layoutManager = new LinearLayoutManager(this);
         activityChatBinding.rvChats.setLayoutManager(layoutManager);
-        messages(user.getUid(),userid);
-        list=new ArrayList<>();
+        messages(user.getUid(), userid);
+        list = new ArrayList<>();
 
 
     }
@@ -98,17 +104,16 @@ public class ChatActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 list.clear();
-                for (DataSnapshot dataSnapshot: snapshot.getChildren())
-                {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     Chat chats = dataSnapshot.getValue(Chat.class);
                     if ((chats.getReceiver().equals(otherID) && chats.getSender().equals(myID)) || (chats.getSender().equals(otherID) && chats.getReceiver().equals(myID)))
                         list.add(chats);
                 }
 
-                if (list.size()==0)
+                if (list.size() == 0)
                     activityChatBinding.startRv.setVisibility(View.VISIBLE);
 
-                adapter=new ChatAdapter(ChatActivity.this,list);
+                adapter = new ChatAdapter(ChatActivity.this, list);
                 activityChatBinding.rvChats.setAdapter(adapter);
                 adapter.notifyDataSetChanged();
             }
@@ -118,6 +123,6 @@ public class ChatActivity extends AppCompatActivity {
 
             }
         });
-        
+
     }
 }
